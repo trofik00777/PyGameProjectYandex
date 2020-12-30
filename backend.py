116c40fc9d8d2d2c2038655d34dfd79a130afed9
trofik00777 <57316041+trofik00777@users.noreply.github.com>
2020-12-30 13:34:20 +0300

    добавил константы для подключения к БД

---
+++ backend.py
@@ -0,0 +1,17 @@
+import pymysql
+import hashlib
+import ftplib
+import os
+import shutil
+
+
+HOST = "f0478423.xsph.ru"
+USER = "f0478423_pygame_wolf_and_eggs"
+PASSWORD = "wolfandeggs"
+DATABASE = "f0478423_pygame_wolf_and_eggs"
+
+HOST_FTP = "141.8.193.236"
+USER_FTP = "f0478423"
+
+PATH_PHOTOS = "C:\\ProgramData\\UCSG"
+SEPARATOR = "\\"
